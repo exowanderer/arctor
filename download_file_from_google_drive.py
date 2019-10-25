149332eb84b52bcb82cbcd0f9df01c80311fecb8
Jonathan <jdfraine@gmail.com>
2019-10-25 22:24:59 +0300

    removed specific keys for download files

--- download_file_from_google_drive.py
+++ download_file_from_google_drive.py
@@ -1,3 +1,5 @@
+import errno
+import os
 import requests
 
 
@@ -32,38 +34,12 @@ def save_response_content(response, destination):
             if chunk:  # filter out keep-alive new chunks
                 f.write(chunk)
 
-if __name__ == "__main__":
-    import os
-    import errno
 
-    def mkdir_p(path):
-        try:
-            os.makedirs(path)
-        except OSError as exc:  # Python >2.5
-            if exc.errno == errno.EEXIST and os.path.isdir(path):
-                pass
-            else:
-                raise
-
-    joblib_file_id = '1x8tu49oIj1f9Sd007s37xK4O4NRTG14b'
-    destination = 'wasp43_savedict_206ppm_100x100_finescale.joblib.save'
-    download_file_from_google_drive(joblib_file_id, destination)
-
-    tar_file_id = '1TscP7ES99XAWeHYT21JJBGbiKn1C8iFs'
-    destination = 'WASP_HST_UVIS_FLT_Fits_Files.tar.gz'
-    download_file_from_google_drive(tar_file_id, destination)
-
-    spitzercal_file_id = '1e4iZ0vLPq9w-TeZJcCIConiXKqsjC89X'
-    destination = 'pmap_ch2_0p1s_x4_rmulti_s3_7.csv'
-    download_file_from_google_drive(tar_file_id, destination)
-
-    # SpitzerCal for the GA-CNNs
-    spitzercal_file_id = '1e4iZ0vLPq9w-TeZJcCIConiXKqsjC89X'
-    HOME = os.environ['HOME']
-    dest_dir = f'{HOME}/.vaelstmpredictor/data/'
-
-    if not os.path.exists(dest_dir):
-        mkdir_p(dest_dir)
-
-    destination = os.path.join(dest_dir, 'pmap_ch2_0p1s_x4_rmulti_s3_7.csv')
-    download_file_from_google_drive(spitzercal_file_id, destination)
+def mkdir_p(path):
+    try:
+        os.makedirs(path)
+    except OSError as exc:  # Python >2.5
+        if exc.errno == errno.EEXIST and os.path.isdir(path):
+            pass
+        else:
+            raise
